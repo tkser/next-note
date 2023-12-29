33dcfd79b33a1ffa23d94958d9f2a0b245d4bf18
tkser <tkser.edu@gmail.com>
2023-12-29 19:09:50 +0900

    add: lock icon note page

--- app/_components/pages/NotePage.tsx
+++ app/_components/pages/NotePage.tsx
@@ -69,11 +69,12 @@ const NotePage = ({ note, author }: NotePageProps) => {
           <ul className="space-y-2 border-2 border-gray-300 p-4">
             {pages.map((page) => (
               <li
-                className="flex items-center space-x-4 mb-2 text-gray-700 border-b-2 border-gray-300 pb-1"
+                className="flex items-center mb-2 text-gray-700 border-b-2 border-gray-300 pb-1 gap-4"
                 key={page.page_id}
               >
-                <span>{page.position.toString().padStart(2, "0")}</span>
-                <Link href="/notes/[noteSlug]/[pageSlug]" as={`/notes/${note.slug}/${page.slug}`}>
+                <span className="w-5">{page.position.toString().padStart(2, "0")}</span>
+                <Link href="/notes/[noteSlug]/[pageSlug]" as={`/notes/${note.slug}/${page.slug}`} className="flex flex-row gap-1 items-center">
+                  <span className="select-none">{page.is_private && <BiSolidLockAlt />}</span>
                   <span>{page.title}</span>
                 </Link>
               </li>
